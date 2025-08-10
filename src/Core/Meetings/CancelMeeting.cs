59d293e5fef2614cae95b1f2ade14cd52a67cb22
LaBani Said <labani.said@outlook.com>
2025-08-10 19:59:18 +0200

    Add CancelMeeting.cs

---
+++ src/Core/Meetings/CancelMeeting.cs
@@ -0,0 +1,11 @@
+﻿namespace Meetings.Meetings;
+
+public class CancelMeeting
+{
+    public CancelMeeting(Guid meetingId)
+    {
+        MeetingId = meetingId;
+    }
+
+    public Guid MeetingId { get; }
+}
