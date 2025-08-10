185a7bbd79d73c037720df6f17af8a7a28bea9eb
LaBani Said <labani.said@outlook.com>
2025-08-10 20:39:23 +0200

    Add RemoveMeetingAttendee.cs

---
+++ src/Core/Meetings/RemoveMeetingAttendee.cs
@@ -0,0 +1,17 @@
+﻿namespace Meetings.Meetings;
+
+internal class RemoveMeetingAttendee
+{
+    public RemoveMeetingAttendee(Guid meetingId, Guid attendeeId, string removingReason)
+    {
+        MeetingId = meetingId;
+        AttendeeId = attendeeId;
+        RemovingReason = removingReason;
+    }
+
+    public Guid MeetingId { get; }
+
+    public Guid AttendeeId { get; }
+
+    public string RemovingReason { get; }
+}
