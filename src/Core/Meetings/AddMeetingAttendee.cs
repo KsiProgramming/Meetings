2845114eb48d5e490b817d30d79da7211aa8671a
LaBani Said <labani.said@outlook.com>
2025-08-10 19:53:46 +0200

    Add Meeting module first implementation

---
+++ src/Core/Meetings/AddMeetingAttendee.cs
@@ -0,0 +1,14 @@
+﻿namespace Meetings.Meetings;
+
+public class AddMeetingAttendee
+{
+    public Guid MeetingId { get; }
+
+    public int GuestsNumber { get; }
+
+    public AddMeetingAttendee(Guid meetingId, int guestsNumber)
+    {
+        MeetingId = meetingId;
+        GuestsNumber = guestsNumber;
+    }
+}
---
+++ src/Core/Meetings/MeetingManager.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Meetings.Meetings;
+
+public class MeetingManager
+{
+    public Task AddAttendee(AddMeetingAttendee command)
+    {
+        // Logic to add an attendee to a meeting
+        // This is a placeholder for the actual implementation
+        Console.WriteLine($"Adding {command.GuestsNumber} attendees to meeting {command.MeetingId}");
+        return Task.CompletedTask;
+    }
+}
