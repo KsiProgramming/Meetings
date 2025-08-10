eb4309b85824c1ccdcb9393d6f47ae17866918fc
LaBani Said <labani.said@outlook.com>
2025-08-10 20:33:06 +0200

    Add GetMeetingAttendee.cs

---
+++ src/Core/Meetings/GetMeetingAttendee.cs
@@ -0,0 +1,11 @@
+﻿namespace Meetings.Meetings;
+
+public class GetMeetingAttendee
+{
+    public GetMeetingAttendee(Guid meetingId)
+    {
+        MeetingId = meetingId;
+    }
+
+    public Guid MeetingId { get; }
+}
